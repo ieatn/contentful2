f48c38b7c5eeb25978bcf66eefbf78668e7bbde5
ieatn <ieatnubcake@gmail.com>
2024-09-10 20:28:20 -0700

    have to move env file outside src folder

--- src/Client.jsx
+++ src/Client.jsx
@@ -1,8 +1,20 @@
 import { createClient } from 'contentful';
 
+const spaceId = process.env.REACT_APP_CONTENTFUL_SPACE_ID;
+const accessToken = process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN;
+
+console.log('Space ID:', spaceId);
+console.log('Access Token:', accessToken);
+
+if (!spaceId || !accessToken) {
+  console.error('Contentful space ID or access token is missing.');
+  console.log('Space ID:', spaceId);
+  console.log('Access Token:', accessToken);
+}
+
 const client = createClient({
-  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
+  space: spaceId,
+  accessToken: accessToken,
 });
 
 export default client;
